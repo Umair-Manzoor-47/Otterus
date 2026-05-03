765b79b2ab88800cbaa232e2f3dca8d2c5ef250a
Umair Manzoor <umairmanzoor546@gmail.com>
2026-05-03 21:17:46 +0500

    Feat:Base Class

---
+++ engine/include/core/Common.h
@@ -0,0 +1,11 @@
+//
+// Created by umair on 5/3/2026.
+//
+#pragma once
+namespace engine {
+    struct BaseDesc
+    {
+    };
+
+}
+
---
+++ engine/src/core/base.cpp
@@ -0,0 +1,11 @@
+//
+// Created by umair on 5/3/2026.
+//
+
+#include <core/base.h>
+
+engine::Base::Base(engine::BaseDesc &desc) {
+}
+engine::Base::~Base()
+{
+}
