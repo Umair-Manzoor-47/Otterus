2481b8945e9d92b94f6fedad6e22fb070f032e69
Umair Manzoor <umairmanzoor546@gmail.com>
2026-05-09 02:22:58 +0500

    Refactoring

--- engine/src/entrypoint/Application.cpp
+++ engine/src/entrypoint/Application.cpp
@@ -25,7 +25,7 @@ void engine::Application::InitWindow() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "Otterus", NULL, NULL);
     if (window == NULL)
     {
         LogError("Failed to create GLFW window");
--- game/core/Flores.cpp
+++ game/core/Flores.cpp
@@ -8,7 +8,10 @@ void Flores::OnStart() {
     LogInfo("Flores started.");
 }
 
-void Flores::OnUpdate() {}
+void Flores::OnUpdate() {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
 
 void Flores::OnShutdown() {
     LogInfo("Flores shutdown.");
