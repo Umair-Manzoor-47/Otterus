f618e13ae797188ff57d16735394d67973ea26d3
Umair Manzoor <umairmanzoor546@gmail.com>
2026-05-13 16:25:32 +0500

    Event System: Dispatcher

---
+++ engine/include/event/Dispatcher.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include <functional>
+#include <core/Common.h>
+#include <event/Event.h>
+#include <algorithm>
+namespace engine{
+class Dispatcher
+{
+
+public:
+    using SubscriberID = ui32;
+    SubscriberID Subscribe(EventType type, std::function<void(Event&)> callback);
+    void Dispatch(Event& event);
+    void Unsubscribe(EventType type, SubscriberID id);
+    
+private:
+    using Callback = std::function<void(Event&)>;
+    using Listener = std::pair<SubscriberID, Callback>;
+    using ListenerList = std::vector<Listener>;
+
+    std::unordered_map<EventType, ListenerList> m_listeners;
+    SubscriberID m_nextID = 0;
+    
+    };
+}
---
+++ engine/src/event/Dispatcher.cpp
@@ -0,0 +1,56 @@
+﻿#include <event/Dispatcher.h>
+
+engine::Dispatcher::SubscriberID engine::Dispatcher::Subscribe(
+    EventType type,
+    Callback callback)
+{
+    SubscriberID id = m_nextID++;
+
+    m_listeners[type].emplace_back(id, std::move(callback));
+
+    return id;
+}
+
+void engine::Dispatcher::Dispatch(Event& event)
+{
+    auto it = m_listeners.find(event.GetType());
+
+    if (it == m_listeners.end())
+        return;
+
+    auto& listeners = it->second;
+
+    for (auto& [id, callback] : listeners)
+    {
+        if (event.Handled) break;
+        if (callback)
+        {
+            callback(event);
+        }
+    }
+}
+
+void engine::Dispatcher::Unsubscribe(EventType type, SubscriberID id)
+{
+    auto it = m_listeners.find(type);
+
+    if (it == m_listeners.end())
+        return;
+
+    auto& listeners = it->second;
+
+    listeners.erase(
+        std::remove_if(
+            listeners.begin(),
+            listeners.end(),
+            [id](const Listener& listener)
+            {
+                return listener.first == id;
+            }),
+        listeners.end());
+    
+    if (listeners.empty())
+    {
+        m_listeners.erase(it);
+    }
+}
