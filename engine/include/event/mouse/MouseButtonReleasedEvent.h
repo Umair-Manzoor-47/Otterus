92500d0741b49f5ee856b57fac5f1a525394451c
Umair Manzoor <umairmanzoor546@gmail.com>
2026-05-15 15:27:12 +0500

    Added Events
    Mouse Button Clicked
    Mouse Moved
    Mouse Scrolled
    Key Released
    Window Resized

---
+++ engine/include/event/mouse/MouseButtonReleasedEvent.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+namespace engine
+{
+    class MouseButtonReleasedEvent : public Event
+    {
+    public:
+        explicit MouseButtonReleasedEvent(int button)
+            : m_button(button) {}
+
+        int GetButton() const { return m_button; }
+
+        EventType GetType() const override { return GetStaticType(); }
+
+        static EventType GetStaticType() {
+            return EventType::MouseButtonReleased;
+        }
+
+    private:
+        int m_button;
+    };
+    
+}
+
---
+++ engine/include/event/window/WindowResizeEvent.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+namespace engine{
+	class WindowResizeEvent: public Event
+	{
+	public:
+   		explicit WindowResizeEvent(const int width, const int height): m_width(width), m_height(height){};
+		int GetWidth() const{return m_width;}
+		int GetHeight() const{return m_height;}
+		
+		EventType GetType() const override { return GetStaticType(); }
+		static EventType GetStaticType() { return EventType::WindowResize; }
+	private:
+		int m_width;
+		int m_height;
+	};
+
+}
